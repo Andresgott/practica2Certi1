a3058bf1a0977d79026e0d5ba16aa9ffd3af86ad
andresGott <ag.gott@gmail.com>
2024-04-30 09:01:26 -0400

    P3-001 added library class Business Logic, model patient and managers PatientManager also createPatient function and randomBloodType function

---
+++ practica2Certi1/BusinessLogic/Managers/PatientManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UPB.BusinessLogic.Models;
+
+namespace UPB.BusinessLogic.Managers
+{
+    public class PatientManager
+    {
+        public PatientManager() { }
+
+        public Patient CreatePatient(string name,string lastName,int ci)
+        {
+            Patient patient;
+            patient = new Patient(name, lastName,ci,randomBloodType());
+
+            return patient;
+
+        }
+
+        private string randomBloodType() 
+        {
+            string bloodType;
+            int rand = new Random().Next(0,81);
+            if(rand < 10) 
+            {
+                bloodType = "A+";
+            }
+            else if(rand < 20) 
+            {
+                bloodType = "A-";
+            }
+            else if(rand < 30) 
+            {
+                bloodType = "B+";
+            }
+            else if (rand < 40)
+            {
+                bloodType = "B-";
+            }
+            else if(rand < 50) 
+            {
+                bloodType = "AB+";
+            }
+            else if (rand < 60)
+            {
+                bloodType = "AB-";
+            }
+            else if (rand < 70)
+            {
+                bloodType = "O+";
+            }
+            else 
+            {
+                bloodType = "O-";
+            }
+            return bloodType;
+        }
+    }
+}
---
+++ practica2Certi1/BusinessLogic/Models/Patient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPB.BusinessLogic.Models
+{
+    public class Patient
+    {
+        public string Name {  get; set; }
+        public string LastName { get; set; }
+        public int CI { get; set; }
+        public string BloodType {  get; set; }
+
+        public Patient() { }
+
+        public Patient(string name, string lastName, int ci,string bloodType) 
+        {
+            Name = name;
+            LastName = lastName;
+            CI = ci;    
+            BloodType = bloodType;
+        }
+    }
+}
